45ceef4fcaba1dd36a3533c830765a3639fea268
Trupti Chandwani <210303105014@paruluniversity.ac.in>
2025-02-19 09:32:16 +0530

    Added frontend to backend connections

--- App/Routs/Website/leaveentitlementroute.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-
-const { leaveentitlement } = require("../../Controller/Website/leaveentitlement");
-
-let leaveentitlementroute =express.Router();
-leaveentitlementroute.post('/leaveentitlement',leaveentitlement)
-
-module.exports={leaveentitlementroute}
